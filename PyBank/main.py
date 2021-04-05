b0e46677f082e2b23b4c623f5e08e649c5575769
Bennet Yousuf <bennetyousuf@bennets-mbp.attlocal.net>
2021-04-05 17:36:31 -0500

    adding in changes to main.py file for PyBank activity, reading in csv file and finding total Month ccount

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os 
 
+#import CSV
 import csv
 
 csvpath =os.path.join('Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
@@ -13,8 +14,27 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}") 
 
+#row counter, total number of rows in the column “Date” output as “Total Months” is desired final output
+    rowcount = 0
     for row in csvreader:
-        print(row) 
+        rowcount += 1
+    print('Total Months: ',rowcount)
+
+#Calculate the total of "Profit/Losses" over Period
+    row_sum = sum('Profits',86) 
+    
+
+
+    
+    
+    
+   # for row in csvreader:
+        #len(row)
+        #row=len(row)+=1
+        #print(row)
+
+
+
 
 
 
