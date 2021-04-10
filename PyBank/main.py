dab98d29d57912333494d023c86f37b6bf5644d2
Bennet Yousuf <bennetyousuf@Bennets-MacBook-Pro.local>
2021-04-10 16:15:18 -0500

    Upload changes from PyBank main.py file and Financial_Analysis output

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,7 @@ import os
 import csv
 
 csvpath =os.path.join('Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
-
+print(csvpath)
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -15,7 +15,6 @@ with open(csvpath) as csvfile:
     #print(f"CSV Header: {csv_header}") 
 
 #row counter, total number of rows in the column “Date” output as “Total Months” is desired final output
-
     rowcount = 0
     total_profit = 0
     months = []
@@ -44,15 +43,38 @@ with open(csvpath) as csvfile:
         monthly_profit_change.append(total_change_profit)
 
 #find average of profit loss over time
-round(sum(monthly_profit_change)/len(monthly_profit_change),2) 
+round(sum(monthly_profit_change)/len(monthly_profit_change),2)  
 
-#find max and min of monthly_profit_changes; .index used to find the index location of largest change but since monthly_profit_change has 85 values and the original column "Date" has 86 values, +1 to compensate for difference in row numbers
+#find min and max average monthly change in profit 
+max(monthly_profit_change)
+min(monthly_profit_change)
+
+#find max and min month of monthly_profit_changes. .index used to find the index location of largest change. Since monthly_profit_change has 85 rows (since it begins on row 2 after taking the difference of the first 2 values) and the original column "Date" has 86 values, we add +1 to the index to compensate for difference in row numbers
 max_monthly_profit_change = monthly_profit_change.index(max(monthly_profit_change))+1
-print(months[max_monthly_profit_change])
+min_monthly_profit_change = monthly_profit_change.index(min(monthly_profit_change))+1
+
+
 
 
 print('Financial Analysis') 
 print('________________________') 
 print('Total Months: ',rowcount)  
 print('Total: ', str(total_profit))
-print('Average Change : $-',str(round(sum(monthly_profit_change)/len(monthly_profit_change),2))) 
+print('Average Change : $',str(round(sum(monthly_profit_change)/len(monthly_profit_change),2))) 
+print('Greatest Increase in Profits:',months[max_monthly_profit_change],' ($',max(monthly_profit_change),')')
+print('Greatest Decrease in Profits:',months[min_monthly_profit_change],' ($',min(monthly_profit_change),')') 
+
+#write output file to .txtfile 
+#attempt to open file "Financial_Analysis.txt", does not exists so will create with read/write ability
+#write stats into the file "Financial_Analysis.txt" that was just created
+
+Financial_Analysis_File = open("Financial_Analysis.txt", "w+")
+Financial_Analysis_File.write(f""" Financial Analysis
+________________________
+Total Months: {rowcount}  
+Total:  {total_profit}
+Average Change : $ {(round(sum(monthly_profit_change)/len(monthly_profit_change),2))}
+Greatest Increase in Profits: {months[max_monthly_profit_change]} (${max(monthly_profit_change)})
+Greatest Decrease in Profits: {months[min_monthly_profit_change]} (${min(monthly_profit_change)})
+
+""")
