2ba32db38578a58c3047b2aebddb24182c98a98e
Bennet Yousuf <bennetyousuf@Bennets-MacBook-Pro.local>
2021-04-10 16:20:21 -0500

    remove unnecessary print() statement from Main.py in PyBank folder

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,7 @@ import os
 import csv
 
 csvpath =os.path.join('Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
-print(csvpath)
+
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
